11e249fd12337cfd452f4dde4392d83b089016e6
unknown <maqingxiong@jiubang.org>
2018-02-26 13:10:20 +0800

    add fnn

---
+++ feedforward_neural_networks/app.js
@@ -0,0 +1,42 @@
+/**
+ * 预测线性方程 y = 3x + 2 的参数
+ * 其中 3 为 Weights, 2 为 Biases 
+ */
+
+// 创建数据集，这里创建5个x，和5个y
+const x_data = dl.tensor1d([0, 1, 2, 3, 4]);
+const a = dl.scalar(3)
+const b = dl.scalar(2)
+const y_data = x_data.mul(a).add(b)
+  
+// 定义"神经元"的权重和biases
+const Weights = dl.variable(dl.randomUniform([1]))
+const Biases = dl.variable(dl.zeros([1]))
+
+// 定义前馈神经网络
+function neural_network(data) {
+    // w·x+b
+	let layer_output = dl.add(dl.mul(data, Weights), Biases)
+ 
+	return layer_output
+}
+
+// 接着定义损失函数和optimizer
+// 损失函数指的是预测值与实际值之间的差别
+// 神经网络的重点就是通过优化器来减少误差，提升参数的准确度
+// 这里用平方差值作为损失函数，用gradient descent作为优化器
+const loss = (pred, label) => pred.sub(label).square().mean()
+const learningRate = 0.01
+const optimizer = dl.train.sgd(learningRate)
+
+
+// 训练模型，训练500次
+for (let i = 0; i < 500; i++) {
+    optimizer.minimize(() => loss(neural_network(x_data), y_data))
+}
+
+
+// 预测出参数Weights和Biases
+console.log(
+    `Weights: ${Weights.dataSync()}, Biases: ${Biases.dataSync()}`
+)
